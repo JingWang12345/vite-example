284b4d079bc45d58c56703154af2ddf149b94ded
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-05-02 13:07:15 +0200

    first commit

---
+++ dog.js
@@ -0,0 +1,20 @@
+export class Dog{
+
+    constructor(name, breed, age, gender, colors, foodName){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.gender = gender;
+        this.colors = colors;
+
+    }
+
+    sayHello(){
+        console.log('bau');
+    }
+}
+
+class Food{
+    constructor(name, diet);
+
+}
